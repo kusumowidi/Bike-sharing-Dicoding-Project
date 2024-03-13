b46ed591d3518f540870b05a9ff693b010497464
kusumowidi <116607461+kusumowidi@users.noreply.github.com>
2024-03-13 22:13:40 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -83,17 +83,18 @@ else:
 
     # Correlation Heatmap
     st.subheader('Correlation Heatmap')
-    fig, ax = plt.subplots(figsize=(10,6))
-    correlation_matrix = day_df.corr()
-    sns.heatmap(
-        correlation_matrix,
-        annot=True,
-        cmap="coolwarm",
-        center=0,
-        fmt=".2f",
-        ax=ax
-    )
-    st.pyplot(fig)
+    with pd.option_context('mode.use_inf_as_null', True):
+        fig, ax = plt.subplots(figsize=(10,6))
+        correlation_matrix = day_df.corr()
+        sns.heatmap(
+            correlation_matrix,
+            annot=True,
+            cmap="coolwarm",
+            center=0,
+            fmt=".2f",
+            ax=ax
+        )
+        st.pyplot(fig)
 
     # Data Visualization Section
     st.header('Data Visualization')
